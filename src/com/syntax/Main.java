c5df12b09b5b71c6a973273277aa9650e1065129
hussainamran <hussainamran1978@gmail.com>
2022-01-25 20:58:34 -0500

    second commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,5 +3,6 @@ package com.syntax;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" I am at the git");
+        System.out.println(" I am batch12");
     }
 }
